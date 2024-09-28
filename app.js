892f4087ff09aeae844cf30b8f82f586e52006b1
abdelkhaleksaadani <abdelkhaleksaadani@gmail.com>
2024-09-28 03:45:59 +0100

    add, delete task

--- app.js
+++ app.js
@@ -1,5 +1,55 @@
-const { Component, mount, xml, useState} = owl;
+const { Component, mount, xml, useState, useRef, onMounted, reactive, useEnv } =
+  owl;
 
+// -------------------------------------------------------------------------
+// Store
+// -------------------------------------------------------------------------
+
+function useStore() {
+  const env = useEnv();
+  return useState(env.store);
+}
+
+// -------------------------------------------------------------------------
+// TaskList
+// -------------------------------------------------------------------------
+class TaskList {
+  constructor(tasks) {
+    this.tasks = tasks || [];
+    const taskIds = this.tasks.map((t) => t.id);
+    this.nextId = taskIds.length ? Math.max(...taskIds) + 1 : 1;
+  }
+
+  addTask(text) {
+    text = text.trim();
+    if (text) {
+      const newTask = {
+        id: this.nextId++,
+        text: text,
+        isCompleted: false,
+      };
+      this.tasks.push(newTask);
+    }
+  }
+
+  toggleTask(task) {
+    task.isCompleted = !task.isCompleted;
+  }
+
+  deleteTask(task) {
+    const index = this.tasks.findIndex((t) => t.id === task.id);
+    this.tasks.splice(index, 1);
+  }
+}
+
+function createTaskStore() {
+  const saveTasks = () =>
+    localStorage.setItem("todoapp", JSON.stringify(taskStore.tasks));
+  const initialTasks = JSON.parse(localStorage.getItem("todoapp") || "[]");
+  const taskStore = reactive(new TaskList(initialTasks), saveTasks);
+  saveTasks();
+  return taskStore;
+}
 // -------------------------------------------------------------------------
 // Task Component
 // -------------------------------------------------------------------------
@@ -7,11 +57,18 @@ class Task extends Component {
   static template = xml/* xml */ `
       <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
         <span><t t-esc="props.task_index+1"/></span>
-        <input type="checkbox" t-att-checked="props.task.isCompleted"/>
-        <span><t t-esc="props.task.text"/></span>
+        <input type="checkbox" t-att-checked="props.task.isCompleted"
+          t-att-id="props.task.id"
+          t-on-click="() => store.toggleTask(props.task)"/>
+        <label t-att-for="props.task.id"><t t-esc="props.task.text"/></label>
+        <span class="delete" t-on-click="() => store.deleteTask(props.task)">🗑</span>
       </div>`;
 
-  static props = ["task","task_index"];
+  static props = ["task", "task_index"];
+
+  setup() {
+    this.store = useStore();
+  }
 }
 
 // -------------------------------------------------------------------------
@@ -20,39 +77,69 @@ class Task extends Component {
 class Root extends Component {
   static template = xml/* xml */ `
     <div class="todo-app">
-      <input placeholder="Add Task" t-on-keyup="addTask" />
+      <input placeholder="Add Task" t-on-keyup="addTask" t-ref="add-input" />
       <div class="task-list" >
-          <t t-foreach="tasks" t-as="task" t-key="task.id">
-              <Task task="task" task_index="task_index" />
+          <t t-foreach="displayedTasks" t-as="task" t-key="task.id">
+              <Task task="task" task_index="task_index"/>
           </t>
       </div>
-    </div>`;
+      <div class="task-panel">
+          <div class="task-counter">
+            <t t-if="displayedTasks.length != store.tasks.length" >
+              <t t-esc="displayedTasks.length"/> / <t t-esc="store.tasks.length"/> tasks
+            </t> 
+          </div>
+          <div>
+            <span 
+              t-foreach="['all', 'active', 'completed']" 
+              t-as="filter" 
+              t-key="filter" 
+              t-att-class="{active: this.filter.value===filter}"
+              t-esc="filter"
+              t-on-click="() => this.setFilter(filter)"/>
+          </div> 
+      </div>
+    </div>
+    `;
   static components = { Task };
+
+  setup() {
+    const inputRef = useRef("add-input");
+    onMounted(() => inputRef.el.focus());
+    this.store = useStore();
+    this.filter = useState({ value: "all" });
+  }
 
   addTask(ev) {
     if (ev.keyCode === 13) {
       const text = ev.target.value.trim();
-      console.log("text entered ",text);
+      this.store.addTask(text);
       ev.target.value = "";
-      this.tasks.push({
-        id: this.tasks.length + 1,
-        text: text,
-        isCompleted: false,
-      });
     }
   }
-  tasks = useState([
-    {
-      id: 1,
-      text: "buy milk",
-      isCompleted: true,
-    },
-    {
-      id: 2,
-      text: "clean house",
-      isCompleted: false,
-    },
-  ]);
+
+  get displayedTasks() {
+    const tasks = this.store.tasks;
+    switch (this.filter.value) {
+      case "active":
+        return tasks.filter((t) => !t.isCompleted);
+      case "completed":
+        return tasks.filter((t) => t.isCompleted);
+      case "all":
+        return tasks;
+    }
+  }
+
+  setFilter(filter) {
+    this.filter.value = filter;
+  }
 }
 
-mount(Root, document.body, { dev: true });
+// -------------------------------------------------------------------------
+// Setup
+// -------------------------------------------------------------------------
+const env = {
+  store: createTaskStore(),
+};
+
+mount(Root, document.body, { dev: true, env });
